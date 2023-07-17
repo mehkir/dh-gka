dd9633f4fa64214fec783ee59691bf2a32814fed
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2023-07-17 08:45:04 +0200

    Add multicast member

--- Member/member.cpp
+++ Member/member.cpp
@@ -3,17 +3,29 @@
 using namespace boost::placeholders;
 
 member::member(boost::asio::io_service& io_service,
+      const boost::asio::ip::address& listen_interface_by_address,
       const boost::asio::ip::address& multicast_address,
-      short multicast_port, bool ping)
+      int multicast_port, bool ping)
     :
       multicast_endpoint_(multicast_address, multicast_port),
-      send_socket_(io_service, multicast_endpoint_.protocol()),
-      multicast_port_(multicast_port),
+      socket_(io_service),
       ping_(ping)
 {
+    // Create the socket and bind it to the multicast address and port
+    boost::asio::ip::udp::endpoint listen_endpoint(
+        listen_interface_by_address, multicast_port);
+    socket_.open(listen_endpoint.protocol());
+    socket_.set_option(boost::asio::ip::udp::socket::reuse_address(true));
+    socket_.bind(listen_endpoint);
+
+    // Join the multicast group.
+    socket_.set_option(
+        boost::asio::ip::multicast::join_group(multicast_address));
+    
     if (ping) {
-        boost::system::error_code dummy;
-        send(dummy);
+      send("Ping");
+    } else {
+      receive();
     }
 }
 
@@ -21,21 +33,40 @@ member::~member()
 {
 }
 
-void member::send(const boost::system::error_code& error) {
-    if (!error)
-    {
-      std::ostringstream os;
-      os << "Ping";
-      message_ = os.str();
+void member::send(std::string message) {
+  std::ostringstream os;
+  os << message;
+  message_ = os.str();
 
-      send_socket_.async_send_to(
-          boost::asio::buffer(message_), multicast_endpoint_,
-          boost::bind(&member::handle_send_to, this,
-            boost::asio::placeholders::error));
-    }
+  socket_.async_send_to(
+      boost::asio::buffer(message_), multicast_endpoint_,
+      boost::bind(&member::handle_send_to, this,
+        boost::asio::placeholders::error));
 }
 
-  void handle_send_to(const boost::system::error_code& error)
-  {
-    // ...
+void member::handle_send_to(const boost::system::error_code& error)
+{
+  if (!error) {
+    receive();
   }
+}
+
+void member::receive() {
+  socket_.async_receive_from(
+  boost::asio::buffer(data_, max_length), sender_endpoint_,
+  boost::bind(&member::handle_receive_from, this,
+        boost::asio::placeholders::error,
+        boost::asio::placeholders::bytes_transferred));
+}
+
+void member::handle_receive_from(const boost::system::error_code& error, size_t bytes_recvd) {
+  if (!error)
+  {
+    boost::asio::ip::udp::endpoint local = socket_.local_endpoint();
+    boost::asio::ip::udp::endpoint sender = sender_endpoint_;
+    std::cout.write(data_, bytes_recvd);
+    std::cout << std::endl;
+    std::string message = ping_ ? "Ping" : "Pong";
+    send(message);
+  }
+}
--- Member/member.hpp
+++ Member/member.hpp
@@ -3,28 +3,29 @@
 #include <sstream>
 #include <boost/asio.hpp>
 #include <boost/bind/bind.hpp>
-#include "boost/date_time/posix_time/posix_time_types.hpp"
 
 class member
 {
 private:
 /* Member variables*/
-  boost::asio::ip::udp::socket send_socket_;
-  boost::asio::ip::udp::socket receive_socket_;
+  boost::asio::ip::udp::socket socket_;
   boost::asio::ip::udp::endpoint sender_endpoint_;
   boost::asio::ip::udp::endpoint multicast_endpoint_;
-  short multicast_port_;
   bool ping_;
   std::string message_;
   enum { max_length = 1024 };
   char data_[max_length];
 private:
 /* Methods */
-  void send(const boost::system::error_code& error);
-    void handle_send_to(const boost::system::error_code& error);
+  void send(std::string message);
+  void handle_send_to(const boost::system::error_code& error);
+  void receive();
+  void handle_receive_from(const boost::system::error_code& error, size_t bytes_recvd);
+
 public:
     member(boost::asio::io_service& io_service,
+      const boost::asio::ip::address& listen_interface_by_address,
       const boost::asio::ip::address& multicast_address,
-      short multicast_port, bool ping);
+      int multicast_port, bool ping);
     ~member();
 };
