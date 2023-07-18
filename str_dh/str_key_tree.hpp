6494e89b0bc6bc516a4e7a61ff8595427a936992
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2023-07-18 17:26:33 +0200

    Add str key tree

---
+++ str_dh/str_key_tree.hpp
@@ -0,0 +1,18 @@
+#include <cryptopp/integer.h>
+#include <memory>
+
+struct str_IN {
+    CryptoPP::Integer group_secret;
+    CryptoPP::Integer blinded_group_secret;
+};
+
+struct str_LN {
+    CryptoPP::Integer member_secret;
+    CryptoPP::Integer blinded_member_secret;
+};
+
+struct str_key_tree {
+    str_IN root_node;
+    str_LN leaf_node;
+    std::unique_ptr<str_key_tree> next_internal_node;
+};
