e05c8f07c32f874c6c25dac03d457b1bb94ca882
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2023-07-19 15:23:20 +0200

    Implement initialization

--- str_dh/member.cpp
+++ str_dh/member.cpp
@@ -1,7 +1,7 @@
 #include "member.hpp"
 #include "MODP2048_256sg.hpp"
 
-member::member() {
+member::member(bool _is_sponsor, service_id_t _service_id) : is_sponsor_(_is_sponsor) {
     diffie_hellman_.AccessGroupParameters().Initialize(p, q, g);
     secret_.New(diffie_hellman_.PrivateKeyLength());
     blinded_secret_.New(diffie_hellman_.PublicKeyLength());
@@ -9,6 +9,15 @@ member::member() {
     diffie_hellman_.GeneratePublicKey(rnd_, secret_, blinded_secret_);
     secret_int_.Decode(secret_.BytePtr(), secret_.SizeInBytes());
     blinded_secret_int_.Decode(blinded_secret_.BytePtr(), blinded_secret_.SizeInBytes());
+    if (is_sponsor_) {
+        offered_service_ = _service_id;
+        member_id = 1;
+        str_key_tree_map_[offered_service_].reset(new str_key_tree());
+        str_key_tree_map_[offered_service_]->root_node.group_secret = secret_int_;
+        str_key_tree_map_[offered_service_]->root_node.blinded_group_secret = blinded_secret_int_;
+    } else {
+        required_service_ = _service_id;
+    }
 }
 
 member::~member() {
--- str_dh/member.hpp
+++ str_dh/member.hpp
@@ -2,8 +2,11 @@
 #define STR_DH_MEMBER
 
 #include "multicast_application_impl.hpp"
+#include "str_key_tree.hpp"
+#include "primitives.hpp"
 #include <cryptopp/dh.h>
 #include <cryptopp/osrng.h>
+#include <map>
 
 class member : public multicast_application_impl {
 private:
@@ -13,8 +16,14 @@ private:
     CryptoPP::SecByteBlock blinded_secret_;
     CryptoPP::Integer secret_int_;
     CryptoPP::Integer blinded_secret_int_;
+    bool is_sponsor_;
+    std::map<service_id_t, std::unique_ptr<str_key_tree>> str_key_tree_map_;
+
+    service_id_t required_service_ = -1;
+    service_id_t offered_service_ = -1;
+    member_id_t member_id = -1;
 public:
-    member();
+    member(bool _is_sponsor, service_id_t _service_id);
     ~member();
     void received_data(void* data, std::size_t bytes_recvd);
     void send(std::string message);
