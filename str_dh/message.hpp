8d44bdfb062b996e16526247634e637560077207
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2023-07-19 15:23:30 +0200

    Add messages

---
+++ str_dh/message.hpp
@@ -0,0 +1,55 @@
+#ifndef STR_DH_MESSAGE
+#define STR_DH_MESSAGE
+
+#include "primitives.hpp"
+#include <cryptopp/integer.h>
+
+enum message_type {
+    FIND,
+    OFFER,
+    REQUEST,
+    RESPONSE
+};
+
+struct message {
+    public:
+        message_type message_type_;
+};
+
+struct find_message : message {
+    public:
+        find_message() {
+            message_type_ = message_type::FIND;
+        }
+        service_id_t required_service_;
+};
+
+struct offer_message : message {
+    public:
+        offer_message() {
+            message_type_ = message_type::OFFER;
+        }
+        service_id_t offered_service_;
+};
+
+struct request_message : message {
+    public:
+        request_message() {
+            message_type_ = message_type::REQUEST;
+        }
+        service_id_t required_service_;
+        CryptoPP::Integer blinded_secret_int_;
+        member_id_t requester_id_;
+};
+
+struct response_message : message {
+    public:
+        response_message() {
+            message_type_ = message_type::RESPONSE;
+        }
+        service_id_t offered_service_;
+        CryptoPP::Integer blinded_member_secret_int_;
+        CryptoPP::Integer blinded_group_secret_int_;
+};
+
+#endif
