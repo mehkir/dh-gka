1e2367cb2b33e6a9231ce2492c38e2dafe89dd2e
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2023-08-18 19:11:31 +0000

    Add map contains endpoint

--- testframe.cpp
+++ testframe.cpp
@@ -6,6 +6,7 @@
 int main () {
     boost::asio::ip::udp::endpoint ep1(boost::asio::ip::address::from_string("1.2.3.4"), 55);
     boost::asio::ip::udp::endpoint ep2(boost::asio::ip::address::from_string("1.2.3.4"), 56);
+    boost::asio::ip::udp::endpoint ep3(boost::asio::ip::address::from_string("1.2.3.5"), 56);
     std::unordered_map<boost::asio::ip::udp::endpoint, int> umap;
     umap[ep1] = 1;
     umap[ep2] = 2;
@@ -21,5 +22,8 @@ int main () {
         std::cout << "not null" << std::endl;
     }
 
+    if (umap.contains(ep3)) {
+        std::cout << "contains" << std::endl;
+    }
     return 0;
 }
