5546ba588647a54d9ea84a8d3384508a63d252c8
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2023-10-12 13:26:52 +0000

    Add member info message

--- key_agreement_protocol/key_agreement_protocol.hpp
+++ key_agreement_protocol/key_agreement_protocol.hpp
@@ -12,6 +12,8 @@ public:
     virtual void process_offer(offer_message _rcvd_offer_message, boost::asio::ip::udp::endpoint _remote_endpoint) = 0;
     virtual void process_request(request_message _rcvd_request_message, boost::asio::ip::udp::endpoint _remote_endpoint) = 0;
     virtual void process_response(response_message _rcvd_response_message, boost::asio::ip::udp::endpoint _remote_endpoint) = 0;
+    virtual void process_member_info_request(member_info_request_message _rcvd_member_info_request_message, boost::asio::ip::udp::endpoint _remote_endpoint) = 0;
+    virtual void process_member_info_response(member_info_response_message _rcvd_member_info_response_message, boost::asio::ip::udp::endpoint _remote_endpoint) = 0;
     virtual void process_distributed_response(distributed_response_message _rcvd_distributed_response_message, boost::asio::ip::udp::endpoint _remote_endpoint) = 0;
 };
 
