157153859608ead19938edde16261c593ccb465a
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2023-10-14 12:45:45 +0000

    Add request delay and target input

--- eval_automization_scripts/start_evaluation.bash
+++ eval_automization_scripts/start_evaluation.bash
@@ -21,21 +21,23 @@ start() {
 
     for (( i=0; i<$(get_subscriber_count $2); i++ ))
     do
-        /root/c++-multicast/build/multicast-dh-example false $1 $2 &
+        /root/c++-multicast/build/multicast-dh-example false $1 $2 $3 $4 $5 &
     done
     while [[ $(get_process_count) != $(get_subscriber_count $2) ]]; do
         echo "Waiting for all subscribers to start up, $(get_process_count)/$(get_subscriber_count $2) are up"
         sleep 1
     done
     echo "All subscribers started up"
+    SLEEP_TIME_FOR_PORT_BOUND_ONCE_CHECK=5
+    echo "Sleeping $SLEEP_TIME_FOR_PORT_BOUND_ONCE_CHECK seconds more for port bound once checks"
+    sleep 5
 
-    /root/c++-multicast/build/multicast-dh-example true $1 $2 &
+    /root/c++-multicast/build/multicast-dh-example true $1 $2 $3 $4 $5 &
     echo "Initial sponsor is started"
-    sleep 1
+    sleep 3
 
     while [[ -n $(pgrep statistics-wr) ]]; do
         sleep 1
-        echo "Waiting for statistics-writer to stop"
     done
     echo "statistics-writer is stopped"
 }
@@ -48,14 +50,14 @@ function on_exit() {
     exit 1
 }
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 5 ]; then
     echo "Not enough parameters" 1>&2
-    echo "Usage: $0 <service_id> <member_count>"
-    echo "Example: $0 42 20"
+    echo "Usage: $0 <service_id> <member_count> <request_delay_min(ms)> <request_delay_max(ms)> <request_count_target>"
+    echo "Example: $0 42 20 10 100 10"
     exit 1
 fi
 
 trap 'on_exit' SIGINT
 
 compile
-start $1 $2
+start $1 $2 $3 $4 $5
