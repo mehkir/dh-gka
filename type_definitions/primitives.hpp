2fed55d3f8e75b2388293a19b333a596c359d9f1
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2023-10-20 12:41:00 +0000

    Change member_id_t to uint16_t

--- type_definitions/primitives.hpp
+++ type_definitions/primitives.hpp
@@ -4,7 +4,7 @@
 #include <cstdint>
 
 typedef int service_id_t;
-typedef int member_id_t;
+typedef uint16_t member_id_t;
 typedef uint8_t message_id_t;
 typedef CryptoPP::SecByteBlock blinded_secret_t;
 typedef CryptoPP::SecByteBlock secret_t;
