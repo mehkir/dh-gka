77d490063da0839178e4f37e695117a68a912f68
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2023-10-24 11:18:24 +0200

    Add loop for multiple runs

--- eval_automization_scripts/start_runs.bash
+++ eval_automization_scripts/start_runs.bash
@@ -6,10 +6,15 @@ SCATTER_DELAY_MAX=100
 CRYPTO_ALGORITHM=('DEFAULT_DH' 'ECC_DH')
 KEY_AGREEMENT_PROTOCOL=('PROTO_DST_DH' 'PROTO_STR_DH')
 
+RUNS=100
+
 for protocol in "${KEY_AGREEMENT_PROTOCOL[@]}"; do
     for algorithm in "${CRYPTO_ALGORITHM[@]}"; do
-        for member_count in {100..1000..100}; do
-            /root/c++-multicast/eval_automization_scripts/start_evaluation.bash $SERVICE_ID $member_count $SCATTER_DELAY_MIN $SCATTER_DELAY_MAX $algorithm $protocol
+        for i in $(seq 1 $RUNS); do
+            echo "Running ${protocol}-${algorithm} ${i}/${RUNS}"
+            for member_count in {100..1000..100}; do
+                /root/c++-multicast/eval_automization_scripts/start_evaluation.bash $SERVICE_ID $member_count $SCATTER_DELAY_MIN $SCATTER_DELAY_MAX $algorithm $protocol 1>/dev/null
+            done
         done
     done
 done
