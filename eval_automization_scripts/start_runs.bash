38ca0149294a5694a25c499c009a8e6d4aa67fe5
Mehmet Mueller <mehmet.mueller@haw-hamburg.de>
2023-11-13 14:00:13 +0100

    Add absolute project and directory path parameter

--- eval_automization_scripts/start_runs.bash
+++ eval_automization_scripts/start_runs.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
-PROJECT_PATH="/home/mehmet/vscode-workspaces"
+ABSOLUTE_PROJECT_PATH="/home/mehmet/vscode-workspaces/c++-multicast"
+ABSOLUTE_RESULTS_DIRECTORY_PATH=${ABSOLUTE_PROJECT_PATH}/statistic_results/test_runs_without_retransmissions
 SERVICE_ID=42
 SCATTER_DELAY_MIN=0
 SCATTER_DELAY_MAX=0
@@ -7,7 +8,7 @@ KEY_AGREEMENT_PROTOCOL=('PROTO_DST_DH' 'PROTO_STR_DH')
 # CRYPTO_ALGORITHM=('DEFAULT_DH' 'ECC_DH')
 CRYPTO_ALGORITHM=('ECC_DH')
 
-RUNS=1
+RUNS=100
 
 for protocol in "${KEY_AGREEMENT_PROTOCOL[@]}"; do
     for algorithm in "${CRYPTO_ALGORITHM[@]}"; do
@@ -15,7 +16,7 @@ for protocol in "${KEY_AGREEMENT_PROTOCOL[@]}"; do
             echo "Running ${protocol}-${algorithm} ${i}/${RUNS}"
             # for member_count in {100..1000..100}; do
             for member_count in {2..26}; do
-                ${PROJECT_PATH}/c++-multicast/eval_automization_scripts/start_evaluation.bash $SERVICE_ID $member_count $SCATTER_DELAY_MIN $SCATTER_DELAY_MAX $algorithm $protocol 1>/dev/null
+                ${ABSOLUTE_PROJECT_PATH}/eval_automization_scripts/start_evaluation.bash $SERVICE_ID $member_count $SCATTER_DELAY_MIN $SCATTER_DELAY_MAX $algorithm $protocol $ABSOLUTE_PROJECT_PATH $ABSOLUTE_RESULTS_DIRECTORY_PATH 1>/dev/null
             done
         done
     done
